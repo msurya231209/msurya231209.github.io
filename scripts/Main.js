f5dc206878371139701765f5859c791cc2da9aa1
msurya231209 <msurya231209@gmail.com>
2017-05-15 10:38:10 +0530

    Secon Web App Upload

---
+++ scripts/Main.js
@@ -0,0 +1,54 @@
+﻿/// <reference path="../Index.html" />
+
+   var dojoConfig = {
+            packages: [{
+                name: "bootstrap",
+                location: "https://esri.github.io/calcite-maps/dist/vendor/dojo-bootstrap"
+            }, {
+                name: "calcite-maps",
+                location: "https://esri.github.io/calcite-maps/dist/js/dojo"
+            }]
+        };
+
+ 
+    require([
+      "esri/arcgis/utils",
+      "esri/dijit/Measurement",
+      "calcite-maps/calcitemaps-v0.3",
+      "bootstrap/Collapse",
+      "bootstrap/Dropdown",
+      "bootstrap/Tab",
+      "dojo/domReady!"
+    ],
+    function (arcgisUtils, Measurement, Button, CalciteMaps) {
+
+        esri.config.defaults.io.proxyUrl = "/proxy/";
+        esri.config.defaults.io.alwaysUseProxy = false;
+        esri.config.defaults.geometryService = new esri.tasks.GeometryService("https://utility.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer");
+
+        arcgisUtils.createMap("a7892820987c499ea376fe558b54eab3", "mapViewDiv").then(function (response) {
+            window.map = response.map;
+
+            var measurement = new Measurement({
+                map: window.map,
+            }, dojo.byId('measurementDiv'));
+            measurement.startup();
+        })
+        //JS code should deploy
+        document.getElementById("btnfb").onclick = function () {
+            window.open("https://www.facebook.com/");
+        }
+        document.getElementById("btnTwitter").onclick = function () {
+            window.open("https://twitter.com/");
+        }
+        document.getElementById("btnGplus").onclick = function () {
+            window.open("https://plus.google.com/");
+        }
+        document.getElementById("btnEmail").onclick = function () {
+            window.open("http://ww8.msoutlook.com/");
+        }
+        document.getElementById("btnLink").onclick = function () {
+            window.open("http://localhost:34129/");
+        }
+    })
+
---
+++ scripts/ShareDialog1.js
@@ -0,0 +1,32 @@
+﻿/// <reference path="../Index.html" />
+
+var mailURL = "https://mail.google.com/mail/u/0/#inbox?compose=new";
+var facebookURL = "https://www.facebook.com/sharer/sharer.php";
+var twitterURL = "https://twitter.com/intent/tweet";
+var googlePlusURL = "https://plus.google.com/share";
+
+url = "http://arcg.is/LrHvH"
+urlLocal = "http://localhost:34129/"
+
+document.getElementById("btnfb").onclick = function () {
+    var fbfullurl = facebookURL + "?u=" + url;
+    window.open(fbfullurl);
+}
+document.getElementById("btnTwitter").onclick = function () {
+    var twfullurl = "https://twitter.com/intent/tweet?url=http://jsbin.com/bemutamojo/1/edit?output&text=MLB%20Ballparks%20by%20Payroll&hashtags=esriPIM";
+    window.open(twfullurl);
+}
+document.getElementById("btnGplus").onclick = function () {
+    var gpfullurl = googlePlusURL + "?url=" + url;
+    window.open(gpfullurl);
+}
+document.getElementById("btnEmail").onclick = function () {
+    //var link = <a href="mailto:narayanamodalavalasa@gmail.com?subject=The%20subject%20of%20the%20mail"></a>
+    link = 'mailto:msurya231209@gmail.com?subject=The%20subject%20of%20the%20mail';
+    //window.location.href = link;
+    //window.open(window.location.href);
+    window.location.href = link;
+}
+document.getElementById("btnLink").onclick = function () {
+    window.open("http://localhost:34129/");
+}
